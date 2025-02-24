165de2e2fdf0047cb0d41136790c31d3d631ef9f
devamanan <manandeva445@gmail.com>
2025-02-24 23:27:54 +0530

    changes in main.tf and .tfstate

--- 03-basics/aws-backend/main.tf
+++ 03-basics/aws-backend/main.tf
@@ -4,13 +4,13 @@ terraform {
   # YOU WILL UNCOMMENT THIS CODE THEN RERUN TERRAFORM INIT
   # TO SWITCH FROM LOCAL BACKEND TO REMOTE AWS BACKEND
   ############################################################
-#   backend "s3" {
-#     bucket         = "my-terraform-state-bucket-2025" # REPLACE WITH YOUR BUCKET NAME
-#     key            = "03-basics/import-bootstrap/terraform.tfstate"
-#     region         = "us-east-1"
-#     dynamodb_table = "terraform-state-locking"
-#     encrypt        = true
-#   }
+  #   backend "s3" {
+  #     bucket         = "my-terraform-state-bucket-2025" # REPLACE WITH YOUR BUCKET NAME
+  #     key            = "03-basics/import-bootstrap/terraform.tfstate"
+  #     region         = "us-east-1"
+  #     dynamodb_table = "terraform-state-locking"
+  #     encrypt        = true
+  #   }
 
   required_providers {
     aws = {
@@ -30,14 +30,14 @@ resource "aws_s3_bucket" "terraform_state_2025" {
 }
 
 resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
-  bucket = aws_s3_bucket.terraform_state.id
+  bucket = aws_s3_bucket.terraform_state_2025.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
-  bucket        = aws_s3_bucket.terraform_state.bucket 
+  bucket = aws_s3_bucket.terraform_state_2025.bucket
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
